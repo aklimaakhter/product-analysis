0af2c50c73e569abd689d953c284743ca33a5fe0
Aminulislamsumon <inthesumon1010@gmail.com>
2022-04-04 12:16:42 +0600

    Routes setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from'./components/Home/Home';
+import Blog from './components/Blog/Blog';
+import Reviews from './components/Reviews/Reviews';
+import Dashboard from './components/Dashboard/Dashboard';
+import About from './components/About/About';
+import NotFound from './components/NotFound/NotFound';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div>
+            <h3>Thdgjfgjdfo</h3>
+        </div>
+    );
+};
+
+export default Blog;
